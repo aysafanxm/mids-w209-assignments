b917beb1f241b8228c4affd020af6ed9880715dc
James Peng <james.y.peng@gmail.com>
2016-10-17 20:27:56 +0800

    code cleanup

--- assignment4/lineWithFocus.js
+++ assignment4/lineWithFocus.js
@@ -3,23 +3,21 @@ d3.csv("https://raw.githubusercontent.com/jamesypeng/mids-w209-assignments/maste
 		return console.log("there was an error loading the csv: " + error);
 	console.log("there are " + csv.length + " elements in my csv set");
 
+
 	var dataset = [
 		{
-			key: "heart_rate(09-01)",
+			key: "Heart Rate(09-01)",
 			area: false,
 			values: []
-		},
-		{
-			key: "heart_rate(09-02)",
+		}, {
+			key: "Heart Rate(09-02)",
 			area: false,
 			values: []
-		},
-		{
-			key: "heart_rate(09-03)",
+		}, {
+			key: "Heart Rate(09-03)",
 			area: false,
 			values: []
-		},
-
+		}
 	];
 
 	function act_data(x, y) {
@@ -30,10 +28,9 @@ d3.csv("https://raw.githubusercontent.com/jamesypeng/mids-w209-assignments/maste
 
 	// populate the empty object with your data
 	csv.forEach(function (d, i) {
-
+		// the + will convertthe string to integer
 		dataset[0].values.push(new act_data(i, +d["09-01"]));
-		console.log(dataset[0].values[i])
-
+		//console.log(dataset[0].values[i])
 
 		dataset[1].values.push(new act_data(i, +d["09-02"]));
 		dataset[2].values.push(new act_data(i, +d["09-03"]));
@@ -41,29 +38,31 @@ d3.csv("https://raw.githubusercontent.com/jamesypeng/mids-w209-assignments/maste
 
 
 	nv.addGraph(function() {
-		var maxY = 170;
-		var chart = nv.models.lineWithFocusChart()
-						.useInteractiveGuideline(true)
-						.forceY([0,maxY]);
+		var maxY = 160;
+		var chart = nv.models.lineWithFocusChart();
 
-
+		chart.useInteractiveGuideline(true)
+			.forceY([0,maxY])
+			.brushExtent([ 1, csv.length - 20])
+			.height(600)
+			.focusHeight(100);
 
 		chart.xAxis
 			.axisLabel("Hours of the Day")
 			.tickFormat(function(d) {
-					console.log(csv[d][""]);
 					return csv[d]["time"];
 			});
 
-		chart.x2Axis.tickFormat(d3.format(',f'));
+		chart.x2Axis
+			.tickFormat(function(d) {
+				return csv[d]["time"];
+			});
 
-		chart.yTickFormat(d3.format(',.2f'));
+		chart.yTickFormat(d3.format(',.f'));
 
 		chart.yAxis
 			.axisLabel('Heart Rate (bpm)');
 
-
-		chart.brushExtent([1,127]);
 
 		d3.select('#chart svg')
 			.datum(dataset)
@@ -74,99 +73,4 @@ d3.csv("https://raw.githubusercontent.com/jamesypeng/mids-w209-assignments/maste
 		return chart;
 	});
 
-/*	function testData() {
-		return stream_layers(3,128,100).map(function(data, i) {
-			var stream_data = {
-				key: 'Stream'+i,
-				area: false,  //i == 1, enable the area shade
-				values: data
-			};
-			console.log(stream_data.values);
-			stream_data = dataset[i];
-			return stream_data;
-		});
-	}*/
 });
-
-
-/*
-d3.csv("https://raw.githubusercontent.com/jamesypeng/mids-w209-assignments/master/assignment3/data2.csv", function(error, csv) {
-	if (error)
-		return console.log("there was an error loading the csv: " + error);
-	console.log("there are " + csv.length + " elements in my csv set");
-
-	// create an empty object that nv is expecting
-	var dataset = [
-		{
-			key: "heart_rate(09-01)",
-			values: []
-		},
-		{
-			key: "heart_rate(09-02)",
-			values: []
-		},
-		{
-			key: "heart_rate(09-03)",
-			values: []
-		},
-
-	];
-
-	function act_data(x, y) {
-		this.x = x;
-		this.y = y;
- }
-
-
-	// populate the empty object with your data
-	csv.forEach(function (d, i) {
-		dataset[0].values.push(new act_data(i, d["09-01"]))
-		dataset[1].values.push(new act_data(i, d["09-02"]))
-		dataset[2].values.push(new act_data(i, d["09-03"]))
-	});
-
-	nv.addGraph(function () {
-
-		var maxY = 170;
-		var chart = nv.models.lineWithFocusChart()
-			        .useInteractiveGuideline(true)
-		            .focusEnable(true) // disable focus
-		            .showYAxis(true)
-		            .margin({bottom: 60})
-		            .forceY([0,maxY]);
-
-
-		 chart.xAxis
-		 .axisLabel('Hours of the Day')
-		 .tickFormat(function(d) {
-		 console.log(csv[d][""]);
-		 return csv[d]["time"];
-		 });
-
-		 chart.x2Axis
-		 .tickFormat(function(d) {
-		 return csv[d]["time"];
-		 });
-
-		 chart.yAxis
-		 .axisLabel('Heart Rate (bpm)')
-		 .tickFormat(d3.format(',.f'));
-
-
-		 chart.y2Axis
-		 .tickFormat(d3.format(',.f'));
-
-		 d3.select('#chart svg')
-		 .datum(dataset)
-		 .transition().duration(500)
-		 .call(chart);
-
-		nv.utils.windowResize(chart.update);
-
-		return chart;
-	});
-
-});
-
-
-*/
